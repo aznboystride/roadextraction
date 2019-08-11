bd0856e0734cf71086a9ae71cb6490fa890eaf8d
aznboystride <Fairnuri.aboshehwa@student.csulb.edu>
2019-08-11 06:48:32 +0000

    add validation function and data parallesim works now

--- train.py
+++ train.py
@@ -6,11 +6,15 @@ import torch.optim as optim
 from datasets.data import Dataset
 from networks.FCN import FCN
 from sys import argv
+from torch.utils.data.sampler import SubsetRandomSampler
+import numpy as np
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Jaccard - Intersection Over Union
 def dice_coeff(outputs, labels):
-    intersection = torch.sum(outputs * labels)
-    sum = torch.sum(outputs + labels)
+    intersection = torch.cuda.torch.sum(outputs * labels)
+    sum = torch.cuda.torch.sum(outputs + labels)
     dice = 2. * intersection / sum
     return dice.mean()
 
@@ -20,9 +24,34 @@ def dice_loss(outputs, labels):
 def bce_dice(outputs, labels):
     return nn.BCELoss()(outputs, labels) + dice_loss(outputs, labels)
 
+def split_loaders(dataset, shuffle=True, valsplit=0.2):
+    size = len(dataset)
+    indices = list(range(size))
+    split = int(np.floor(size * valsplit))
+    if shuffle:
+        np.random.shuffle(indices)
+    train_indices = indices[:split]
+    valid_indices = indices[split:]
+    train_sampler, valid_sampler = SubsetRandomSampler(train_indices), SubsetRandomSampler(valid_indices)
+    return torch.utils.data.DataLoader(dataset, batch_size=batch_size, sampler=train_sampler), torch.utils.data.DataLoader(dataset, batch_size=len(dataset), sampler=valid_sampler)
+
+def valid(model, loader):
+    total_loss = 0
+    total_acc = 0
+    counter = 1
+    for (input_batch, label_batch) in loader:
+        input_batch = input_batch
+        label_batch = label_batch
+        optimizer.zero_grad()
+        output_batch = model(input_batch)    
+        loss = criterion(output_batch, label_batch)
+        total_loss += loss.item()
+        total_acc += accuracy(output_batch, label_batch) 
+        print("data {}/{}\tloss {:.5f}\tacc {:.5f}".format(counter*batch_size, batch_size*len(loader), total_loss / counter, total_acc / counter)) 
+        counter += 1
+
 accuracy = lambda x, y: dice_coeff(x, y)
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 train_dir = "data/train"
 
 shuffle = True
@@ -33,6 +62,7 @@ is_deconv = True
 fcn_loss = bce_dice
 
 network = FCN(is_deconv=is_deconv).cuda()
+network = nn.DataParallel(network)
 criterion = fcn_loss
 optimizer = optim.Adam(network.parameters(), lr=lr)
 
@@ -43,16 +73,16 @@ for filename in os.listdir(train_dir):
 
 
 dataset = Dataset(train_dir, image_filenames)
-dataloader = torch.utils.data.DataLoader(dataset, batch_size, shuffle=shuffle)
+train_loader, valid_loader = split_loaders(dataset)
 
 # Training
 for epoch in range(1, epochs+1):
     total_loss = 0
     total_accuracy = 0
     counter = 0
-    for i, (image, mask) in tqdm.tqdm(enumerate(dataloader)):
-        input_batch = image.to(device=device, dtype=torch.float32)
-        label_batch = mask.to(device=device, dtype=torch.float32)
+    for i, (image, mask) in tqdm.tqdm(enumerate(train_loader)):
+        input_batch = image.cuda()
+        label_batch = mask.cuda()
         optimizer.zero_grad()
         output_batch = network(input_batch)
         loss = criterion(output_batch, label_batch)
@@ -63,4 +93,6 @@ for epoch in range(1, epochs+1):
         total_accuracy += accuracy(output_batch, label_batch)
         counter += 1
         if counter % 20 == 0:
-            print("epoch {}/{}\tbatch {}/{}\tloss {:.5f}\taccuracy {:.5f} ".format(epoch, epochs, i+1, len(dataloader), total_loss / counter,total_accuracy/counter))
+            print("epoch {}/{}\tbatch {}/{}\tloss {:.5f}\taccuracy {:.5f} ".format(epoch, epochs, i+1, len(train_loader), total_loss / counter,total_accuracy/counter))
+    print("\nValidating...")
+    valid(network, valid_loader)
